df428279ebc18dc651b5d759ab20543d86aa17f3
Paulo Henrique <contato@paulohenriquesn.com>
2026-02-16 21:29:26 -0300

    feat: add sln and new project producer

---
+++ ProducerApi/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProducerApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
